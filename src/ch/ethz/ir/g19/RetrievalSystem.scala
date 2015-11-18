9278b4348dfe7bec82b39ae22b39dc721431eddc
florangmehlin <florangmehlin@vpn-global-046-dhcp.ethz.ch>
2015-11-18 15:34:19 +0100

    fixed the tinyIR stuff

--- src/ch/ethz/ir/g19/RetrievalSystem.scala
+++ src/ch/ethz/ir/g19/RetrievalSystem.scala
@@ -1,7 +1,12 @@
 package ch.ethz.ir.g19
 
+import ch.ethz.dal.tinyir.io._
+
 object RetrievalSystem {
   def main(args: Array[String]) {
     println("hello world")
+    
+    val docs = new ZipDirStream("")
+    
   }
 }
