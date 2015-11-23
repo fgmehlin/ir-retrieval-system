3fae8cc77c770b422cdea6755acc6101a367b50c
Floran Gmehlin <florangmehlin@Florans-MacBook-Air.local>
2015-11-23 14:03:32 +0100

    new version of TinyIR

--- src/ch/ethz/ir/g19/RetrievalSystem.scala
+++ src/ch/ethz/ir/g19/RetrievalSystem.scala
@@ -1,12 +1,26 @@
 package ch.ethz.ir.g19
 
 import ch.ethz.dal.tinyir.io._
+import java.io.File
 
 object RetrievalSystem {
+  
+  
   def main(args: Array[String]) {
-    println("hello world")
+    val path = "tipster/zips"
     
-    val docs = new ZipDirStream("")
+//    println(new File(".").getAbsolutePath())
+//    val docs = new ZipDirStream(path)
+//    println("Reading from path = " + path)
+//    println("Number of documents = " + docs.length)
+//    val zipList = docs.ziplist
+    
+    val tipS = new TipsterStream(path)
+    //tipS.
+    val xmlDoc = tipS.stream.foreach { x => println(x.name) }
+    
+
+    
     
   }
 }
