61fcd7aa083252ad24d8d08e5c672b8008d15f8a
Alvaro Marco <amarco90@gmail.com>
2015-11-24 05:15:52 -0800

    Add more stopwords to list, method to remove stopwords

--- src/ch/ethz/dal/tinyir/processing/StopWords.scala
+++ src/ch/ethz/dal/tinyir/processing/StopWords.scala
@@ -1,6 +1,10 @@
 package ch.ethz.dal.tinyir.processing
 
 object StopWords {
-  val stopWords = Set("the", "a", "is", "it", "are")
+  val stopWords = Set("", "a", "i", "at", "by", "it", "in", "is", "of", "on",
+                      "or", "to", "and", "are", "for", "the", "from", "that",
+                      "with")
   def filter(tokens : Seq[String]) = tokens.filter(stopWords)
+  def remove(tokens : Seq[String]) =
+    tokens.filter(!stopWords.contains(_)).toList
 }
