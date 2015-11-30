7650da65297114de93702ef4ee5ab7a1c92f0d57
Alvaro Marco <amarco90@gmail.com>
2015-11-30 07:09:52 -0800

    More scala-like QueryReader

--- src/ch/ethz/ir/g19/QueryReader.scala
+++ src/ch/ethz/ir/g19/QueryReader.scala
@@ -3,21 +3,12 @@ package ch.ethz.ir.g19
 import io.Source
 import ch.ethz.dal.tinyir.alerts._
 
-/**
- * @author florangmehlin
- */
 object QueryReader {
-  var queries = List[Query]()
   def readQuery(path: String): List[Query] = {
-    val topicRegex = ""
-    for (line <- Source.fromFile(path).getLines()) {
-      if (line.contains("<title>")) {
-        val lineArray = line.split(":\\s+")
-        println(lineArray.apply(1))
-        queries ::= new Query(lineArray.apply(1))
-      }
-
-    }
-    return queries
+    Source.fromFile(path)
+        .getLines
+        .filter(_.startsWith("<title>"))
+        .map(l => new Query(l.split("<title> Topic:\\s+").apply(1)))
+        .toList
   }
 }
