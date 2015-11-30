8fcc4e88f8f26c325252525f533987af63b911f6
Alvaro Marco <amarco90@gmail.com>
2015-11-30 10:29:37 -0800

    Add "-" to the list of stopwords

--- src/ch/ethz/dal/tinyir/processing/StopWords.scala
+++ src/ch/ethz/dal/tinyir/processing/StopWords.scala
@@ -1,9 +1,9 @@
 package ch.ethz.dal.tinyir.processing
 
 object StopWords {
-  val stopWords = Set("", "a", "i", "at", "by", "it", "in", "is", "of", "on",
-                      "or", "to", "and", "are", "for", "the", "from", "that",
-                      "with")
+  val stopWords = Set("", "-", "a", "i", "at", "by", "it", "in", "is", "of",
+                      "on", "or", "to", "and", "are", "for", "the", "from",
+                      "that", "with")
   def filter(tokens : Seq[String]) = tokens.filter(stopWords)
   def remove(tokens : Seq[String]) =
     tokens.filter(!stopWords.contains(_)).toList
