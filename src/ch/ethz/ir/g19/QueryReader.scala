18f9602e02f4a306e94f3b26ca1ba30aede30402
Floran Gmehlin <florangmehlin@vpn-global-dhcp2-114.ethz.ch>
2015-11-30 16:44:00 +0100

    new QueryReader is back

--- src/ch/ethz/ir/g19/QueryReader.scala
+++ src/ch/ethz/ir/g19/QueryReader.scala
@@ -3,20 +3,12 @@ package ch.ethz.ir.g19
 import io.Source
 import ch.ethz.dal.tinyir.alerts._
 
-/**
- * @author florangmehlin
- */
 object QueryReader {
-  var queries = List[Query]()
   def readQuery(path: String): List[Query] = {
-    val topicRegex = ""
-    for (line <- Source.fromFile(path).getLines()) {
-      if (line.contains("<title>")) {
-        val lineArray = line.split(":\\s+")
-        queries ::= new Query(lineArray.apply(1))
-      }
-
-    }
-    return queries
+    Source.fromFile(path)
+        .getLines
+        .filter(_.startsWith("<title>"))
+        .map(l => new Query(l.split("Topic:\\s+").apply(1).trim))
+        .toList
   }
 }
