a0528211cad6e994c7e735f6ed8cb7da276b7a11
Floran Gmehlin <floran.gmehlin@gmail.com>
2015-12-01 18:57:24 +0100

    new UGLY query builder

--- src/ch/ethz/ir/g19/QueryReader.scala
+++ src/ch/ethz/ir/g19/QueryReader.scala
@@ -2,29 +2,47 @@ package ch.ethz.ir.g19
 
 import io.Source
 import ch.ethz.dal.tinyir.alerts._
-
+import scala.collection.mutable.ArrayBuffer
 object QueryReader {
   def readQrels(path: String) = {
     Source.fromFile(path)
-        .getLines
-        .filter(_.endsWith("1"))
-        .toList
-        .map(_.split(" "))
-        .groupBy(_.head)
-        .mapValues(l => l.map(_.apply(2).replaceAll("-", "")).toSet)
+      .getLines
+      .filter(_.endsWith("1"))
+      .toList
+      .map(_.split(" "))
+      .groupBy(_.head)
+      .mapValues(l => l.map(_.apply(2).replaceAll("-", "")).toSet)
   }
 
   def readQuery(path: String) = {
+    val queries = ArrayBuffer[Query]()
+    var query = ""
+    var topicFound = false
+
     val nums = Source.fromFile(path)
-        .getLines
-        .filter(_.startsWith("<num>"))
-        .map(l => l.split("Number:\\s+").last.trim.toInt)
-        .toList
-    val queries = Source.fromFile(path)
+      .getLines
+      .filter(_.startsWith("<num>"))
+      .map(l => l.split("Number:\\s+").last.trim.toInt)
+      .toList
+    /* val queries = Source.fromFile(path)
         .getLines
         .filter(_.startsWith("<title>"))
-        .map(l => new Query(l.split("Topic:\\s+").last.trim))
-        .toList
-    queries.zip(nums)
+        .map(l => new Query(v))
+        .toList*/
+    for (line <- Source.fromFile(path).getLines()) {
+      if (topicFound) {
+        if (!line.isEmpty())
+          query = query + " " + line.trim()
+        else {
+          queries += new Query(query)
+          topicFound = false
+        }
+      }
+      if (line.startsWith("<title>")) {
+        query = line.split("Topic:\\s+").last.trim
+        topicFound = true
+      }
+    }
+    queries.toList.zip(nums)
   }
 }
