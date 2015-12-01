d15f0201720b7503eb26c467522bc547bd5e884b
Alvaro Marco <amarco90@gmail.com>
2015-11-30 16:22:45 -0800

    Metrics FIXED!

--- src/ch/ethz/ir/g19/Ranking.scala
+++ src/ch/ethz/ir/g19/Ranking.scala
@@ -3,7 +3,6 @@ package ch.ethz.ir.g19
 import scala.collection.mutable.ArrayBuffer
 import scala.collection.mutable.PriorityQueue
 import ch.ethz.dal.tinyir.alerts.Query
-import ch.ethz.dal.tinyir.lectures.PrecisionRecall
 
 object MinOrder extends Ordering[Tuple2[Double, String]] {
   def compare(x: Tuple2[Double, String], y: Tuple2[Double, String]) =
@@ -16,6 +15,35 @@ object MinOrder extends Ordering[Tuple2[Double, String]] {
  */
 class Ranking(n: Int, nq: Int) {
   val r = List.fill(nq)(PriorityQueue.empty(MinOrder))
+
+  def printMetrics(queries: List[Int], relevant: Map[String, Set[String]]) = {
+    val precisions =  ArrayBuffer[Double]()
+    val recalls =  ArrayBuffer[Double]()
+    val APs =  ArrayBuffer[Double]()
+    val retrieved = sortByScore.map(l => l.map(_._2))
+    for ((q, i) <- queries.zipWithIndex) {
+      val currentRetrieved = retrieved.apply(i)
+      val currentRelevant = relevant.get(q + "").get
+      val TP = (currentRetrieved.toSet & currentRelevant).size.toDouble
+      precisions += TP / currentRetrieved.size
+      recalls += TP / currentRelevant.size
+      val precision = currentRetrieved
+          .zipWithIndex
+          .filter{case (doc, _) => currentRelevant.contains(doc)}
+          .zipWithIndex
+          .map(t => (t._2 + 1.0) / (t._1._2 + 1.0))
+      APs += precision.sum / Math.min(currentRelevant.size, 100)
+    }
+    val alpha = 0.5
+    val avgP = (precisions.sum / precisions.size)
+    val avgR = (recalls.sum / recalls.size)
+    val fScores = precisions.zip(recalls)
+        .map(t => 1.0/(alpha / t._1 + (1.0 - alpha) / t._2))
+    val avgF = (fScores.sum / fScores.size)
+    val MAP = APs.sum / queries.size
+
+    println("P=" + avgP + " R=" + avgR + " F=" + avgF + " MAP=" + MAP)
+  }
 
   def processScores(name: String, scores: List[Double]) = {
     for (i <- 0 until nq) {
@@ -30,28 +58,15 @@ class Ranking(n: Int, nq: Int) {
     }
   }
 
-  override def toString() = {
+  /** Creates list of lists with the rankings sorted by score */
+  def sortByScore() = {
     val l = ArrayBuffer[List[Tuple2[Double, String]]]()
     r.foreach(pq => l.append(pq.toList))
-    val sortedl = l.map(_.sortBy(x => -x._1))
-    sortedl.toList.mkString("", "\n", "")
+    l.map(_.sortBy(x => -x._1)).toList
   }
 
-  def printMetrics(queries: List[Int], relevant: Map[String, Set[String]]) = {
-    var precRec =  List[(Double, Double)]()
-    for ((q, i) <- queries.zipWithIndex) {
-      val retrieved = r.apply(i).map(_._2).toSet
-      val pr = PrecisionRecall.evaluate(retrieved, relevant.get(q + "").get)
-      val tuple = (pr.precision, pr.recall)
-      precRec ::= tuple
-    }
-    val alpha = 0.5
-    val f1List = precRec.map(t => 1.0/(alpha / t._1 + (1.0 - alpha) / t._2))
-    val avgP = (precRec.map(_._1).sum / precRec.size)
-    val avgR = (precRec.map(_._2).sum / precRec.size)
-    val avgF1 = (f1List.sum/f1List.size)
-    val MAP = avgF1.toDouble / r.size
-    
-    println("P=" + avgP + " R=" + avgR + " F=" + avgF1 + " MAP=" + MAP)
+  override def toString() = {
+    val sortedl = sortByScore
+    sortedl.toList.mkString("\n")
   }
 }
